22cff0fdfa3fb271f7e4477edae413643fbea0db
michela-andrea <michelayandrea@gmail.com>
2026-07-18 15:21:53 +0200

    translations.js

---
+++ translations.js
@@ -0,0 +1,37 @@
+/* =========================
+   MICHELA & ANDREA WEDDING
+   Traducciones
+========================= */
+
+
+const translations = {
+
+
+    es: {
+
+        welcome: "Nuestra boda",
+
+        program: "Programa del día"
+
+    },
+
+
+    it: {
+
+        welcome: "Il nostro matrimonio",
+
+        program: "Programma della giornata"
+
+    },
+
+
+    en: {
+
+        welcome: "Our wedding",
+
+        program: "Schedule of the day"
+
+    }
+
+
+};
